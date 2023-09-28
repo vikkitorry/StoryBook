01796376ef983d680dc9631fbd2ad6aa628a00f3
vikkitorry <vikkitorry6716@gmail.com>
2023-09-28 14:02:53 +0700

    feat: add switch dev/prod modes

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,23 +1,24 @@
 import webpack from "webpack";
 import path from "path";
 import { buildWebpackConfig } from "./config/build/buildWebpackConfig";
-import { BuildPath } from "./config/build/types/configs";
+import { BuildEnv, BuildPath } from "./config/build/types/configs";
 
-const paths: BuildPath = {
-  entry: path.resolve(__dirname, 'src', 'index.ts'),
-  build: path.resolve(__dirname, 'build'),
-  html: path.resolve(__dirname, 'public', 'index.html'),
-}
+export default (env: BuildEnv) => {
+  const paths: BuildPath = {
+    entry: path.resolve(__dirname, 'src', 'index.ts'),
+    build: path.resolve(__dirname, 'build'),
+    html: path.resolve(__dirname, 'public', 'index.html'),
+  }
 
-const mode = 'development';
-const isDev = mode === 'development';
-const PORT = 3000;
+  const mode = env.mode || 'development';
+  const isDev = mode === 'development';
+  const PORT = env.port || 3000;
 
-const config: webpack.Configuration = buildWebpackConfig({
-  mode: 'development',
-  paths,
-  isDev,
-  port: PORT,
-})
-
-export default config;
+  const config: webpack.Configuration = buildWebpackConfig({
+    mode,
+    paths,
+    isDev,
+    port: PORT,
+  })
+  return config;
+};
