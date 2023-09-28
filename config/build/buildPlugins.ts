422d4cf73b5a61c44f39e44f010db68bbf5c81c2
vikkitorry <vikkitorry6716@gmail.com>
2023-09-28 12:59:49 +0700

    feat: decompose config

---
+++ config/build/buildPlugins.ts
@@ -0,0 +1,13 @@
+import path from "path";
+import webpack from "webpack";
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import { BuildOptions } from "./types/configs";
+
+export function buildPlugins({paths}: BuildOptions): webpack.WebpackPluginInstance[] {
+  return [
+    new HtmlWebpackPlugin({
+      template: paths.html,
+    }),
+    new webpack.ProgressPlugin(),
+  ]
+}
