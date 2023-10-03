ed181f94ab80b206b87fe416e46046294d9c3c29
vikkitorry <vikkitorry6716@gmail.com>
2023-10-03 12:31:15 +0700

    fix: path for classNames

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,7 +2,7 @@ import React, { Suspense } from "react";
 import { Link, Route, Routes } from "react-router-dom";
 import "./styles/index.scss";
 import { useTheme } from "./providers/themeProvider/ui";
-import { classNames } from "helpers/classNames/classNames";
+import { classNames } from "shared/lib/classNames/classNames";
 import { AboutPage } from "pages/AboutPage";
 import { MainPage } from "pages/MainPage";
 
