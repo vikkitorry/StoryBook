152fd94c0404e4551dd9c736fd474237d8eb2168
vikkitorry <vikkitorry6716@gmail.com>
2023-10-04 15:37:43 +0700

    feat: add navbar

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,16 +3,16 @@ import "./styles/index.scss";
 import { useTheme } from "./providers/themeProvider/ui";
 import { classNames } from "shared/lib/classNames/classNames";
 import { AppRouter } from "./providers/router";
+import { Navbar } from "widgets/Navbar";
 
 const App = () => {
   const { theme, toggleTheme } = useTheme();
   const bool = true;
   return (
     <div className={classNames("app", {}, [theme])}>
-      <button onClick={toggleTheme}>TOGGLE</button>
-      <Link to={"/"}>Главная</Link>
-      <Link to={"/about"}>О сайте</Link>
+      <Navbar/>
       <AppRouter/>
+      <button onClick={toggleTheme}>TOGGLE</button>
     </div>
   );
 };
