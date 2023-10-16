023941aa689e8e63e87862155aacb49a5be770f4
vikkitorry <vikkitorry6716@gmail.com>
2023-10-16 13:06:42 +0700

    feat: add stories

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,7 +4,7 @@ import { classNames } from 'shared/lib/classNames/classNames';
 import { AppRouter } from 'app/providers/router';
 import { Navbar } from 'widgets/Navbar';
 import { Sidebar } from 'widgets/Sidebar';
-import { useTheme } from './providers/themeProvider';
+import { useTheme } from 'app/providers/ThemeProvider';
 
 function App() {
     const { theme } = useTheme();
--- src/shared/lib/classNames/classNames.test.ts
+++ src/shared/lib/classNames/classNames.test.ts
@@ -1,4 +1,4 @@
-import { classNames } from './classNames';
+import { classNames } from 'shared/lib/classNames/classNames';
 
 describe('classNames', () => {
     test('with only first param', () => {
@@ -11,20 +11,20 @@ describe('classNames', () => {
             .toBe(expected);
     });
 
-    test('with mods false', () => {
-        const expected = 'someClass class1 class2 hovered';
-        expect(classNames(
-            'someClass',
-            { hovered: true, scrollable: false },
-            ['class1', 'class2'],
-        )).toBe(expected);
-    });
-
     test('with mods', () => {
         const expected = 'someClass class1 class2 hovered scrollable';
         expect(classNames(
             'someClass',
             { hovered: true, scrollable: true },
+            ['class1', 'class2'],
+        )).toBe(expected);
+    });
+
+    test('with mods false', () => {
+        const expected = 'someClass class1 class2 hovered';
+        expect(classNames(
+            'someClass',
+            { hovered: true, scrollable: false },
             ['class1', 'class2'],
         )).toBe(expected);
     });
